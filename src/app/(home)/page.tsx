90c4c6d45aaf18e87467fe71e95ca3b6c9153464
rajatgits <rajatrajemails@gmail.com>
2024-12-08 11:28:42 +0530

    Templates gallery added

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
-import Link from "next/link";
 import { Navbar } from "./navbar";
+import { TemplatesGallery } from "./templates-gallery";
 
 export const Home = () => {
   return (
@@ -8,8 +8,7 @@ export const Home = () => {
         <Navbar />
       </div>
       <div className="mt-16">
-        Rajat Raj
-        <Link href="/documents/123">Click</Link>
+        <TemplatesGallery />
       </div>
     </div>
   );
